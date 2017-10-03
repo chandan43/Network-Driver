4656ba82c29775fab6e63a906007e36601973f7f
Chandan <jha2011eec43@gmail.com>
2017-10-04 02:19:11 +0530

    Enabling the Device

--- rtl8139.c
+++ rtl8139.c
@@ -4,16 +4,35 @@
 
 #define REALTEK_VENDER_ID  0x10EC
 #define REALTEK_DEVICE_ID   0x8139
+static struct pci_dev *rtl8139_probe(void){
+	struct pci_dev *pdev=NULL;
+	/* Look for RealTek 8139 NIC */
+	/*begin or continue searching for a PCI device by vendor/device id. struct pci_dev *pci_get_device(unsigned int vendor, unsigned int device,struct pci_dev *from);*/
+	pdev=pci_get_device(REALTEK_VENDER_ID,REALTEK_DEVICE_ID,NULL);
+	if(pdev){
+		/* device found, enable it */
+		if(pci_enable_device(pdev)){                                           /*0 on success; error code otherwise */
+			pr_err("PCI DEVICE: Couln't able to device\n");
+			return NULL;
+		}
+		else
+			pr_info("PCI DEVICE: Device Eanbled\n");
+		
+	}
+	else{
+		pr_err("REALTEK Device not found\n");
+		return pdev;
+	}
+	return pdev;
+}
+
 
 int __init rtl8139_init(void){
 	struct pci_dev *pdev;
-	printk("%s: Initialization of Network Device driver\n",__func__);
-	/*begin or continue searching for a PCI device by vendor/device id. struct pci_dev *pci_get_device(unsigned int vendor, unsigned int device,struct pci_dev *from);*/
-	pdev=pci_get_device(REALTEK_VENDER_ID,REALTEK_DEVICE_ID,NULL);
+	printk("%s: Initialization of REALTEK Network Device driver\n",__func__);
+	pdev=rtl8139_probe();
 	if(!pdev)
-		pr_err("rtl8139: Device not found\n");
-	else
-		pr_info("rtl8139: Device found\n");
+		return 0;
 	return 0;
 }
 
@@ -26,9 +45,10 @@ void __exit rtl8139_exit(void){
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("beingchandanjha@gmail.com");
 MODULE_DESCRIPTION("Basic Network driver");
+MODULE_VERSION(".1");
 
 module_init(rtl8139_init);
 module_exit(rtl8139_exit);
 
 
-
+/*https://github.com/profglavcho/mt6577-kernel-3.10.65/blob/master/Documentation/PCI/pci.txt*/
